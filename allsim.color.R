839d7c6998c6b81b91336be09b4bf64d64ae608c
Ian Holmes <ihh@berkeley.edu>
2017-03-07 13:44:30 -0800

    allsim.color.pdf

--- allsim.color.R
+++ allsim.color.R
@@ -4,5 +4,5 @@ data$Terms <- cut (data$terms, breaks=c(-Inf,1.5,2.5,3.5,Inf), labels=c("1","2",
 data$Method[data$method == "model"] = "Bayesian"
 data$Method[data$method == "hypergeometric"] = "Frequentist"
 data$fpr <- 1 - data$specificity
-ggplot(aes(x = fpr, y = recall, color = Method, shape = Terms, group = interaction(Method,Terms)), data = data) + geom_point() + xlab("1 - Specificity") + ylab("Recall") + theme(legend.position = c(.88,.25))
+ggplot(aes(x = fpr, y = recall, shape = Method, color = Terms, group = interaction(Method,Terms)), data = data) + geom_point() + xlab("1 - Specificity") + ylab("Recall") + theme(legend.position = c(.88,.25))
 ggsave("allsim.color.pdf")
